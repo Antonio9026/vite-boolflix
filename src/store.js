33650193ced56140435def6be6253bb8be89d5fd
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-08-29 09:46:05 +0200

    cooretto query e apy key in oggetto

--- src/store.js
+++ src/store.js
@@ -8,11 +8,11 @@ export const store = reactive({
 
 export function searchMovie() {
     axios
-        .get(`https://api.themoviedb.org/3/search/movie?api_key=ce632c45436b38c696f8d2038a9db232&query=${store.searchMovie}`, {
-            // params: {
-            //     api_key: "ce632c45436b38c696f8d2038a9db232",
-            //     query: store.searchMovie,
-            // }
+        .get(`https://api.themoviedb.org/3/search/movie`, {
+            params: {
+                api_key: "ce632c45436b38c696f8d2038a9db232",
+                query: store.searchMovie,
+            }
         })
         .then((response) => {
             store.movieList = response.data.results;
